5af2b807aee28a9ea6ce4c26a5e153f14137a97a
mark76729 <weichen729@gmail.com>
2017-12-31 06:15:28 -0800

    firist commit

---
+++ mysite/trips/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+# Create your views here.
+
+
+
+def hello_world(request):
+
+
+    return render(request,'hello_world.html',{'current_time':str(datetime.now()),})
